48686d95f23e6c6cded29929b69884f2fbe1e5d8
Tanzeem <mohammed.tanzeem@enhancesys.com>
2024-11-11 10:36:14 +0530

    Your commit message

---
+++ App.js
@@ -0,0 +1,142 @@
+import { useEffect, useState } from "react";
+import { NativeModules, View, PermissionsAndroid, Text, StyleSheet, Button, ToastAndroid } from "react-native"
+import DeviceInfo from "react-native-device-info";
+import NetInfo from '@react-native-community/netinfo';
+
+const App = props => {
+  const { Telephony } = NativeModules;
+
+  const [cellInfo, setCallInfo] = useState([]);
+  const [carrier, setCarrier] = useState("");
+  const [networkInfo, setNetWorkInfo] = useState({});
+
+  useEffect(() => {
+    const removeNetInfo = NetInfo.addEventListener(state => {
+      setNetWorkInfo({
+        type: state.type,
+        isConnected: state.isConnected
+      })
+    });
+
+    return () => removeNetInfo;
+  }, [])
+
+  useEffect(() => fetchTacAndLac(), [networkInfo]);
+
+  const fetchTacAndLac = () => {
+    if (networkInfo.isConnected && networkInfo.type != 'wifi') {
+      requestPermissions(flag => {
+        if (flag) {
+          DeviceInfo.getCarrier().then((carrier) => {
+            Telephony.getCellInfo(info => {
+              const list = [];
+              info.forEach(element => {
+                const { cellIdentity, connectionType } = element;
+                if (cellIdentity.tac && list.some(obj => obj.tac == cellIdentity.tac)) return;
+                if (cellIdentity.lac && list.some(obj => obj.lac == cellIdentity.lac)) return;
+                list.push({ ...cellIdentity, connectionType });
+              });
+              setCarrier(carrier);
+              setCallInfo(list)
+              // setCarrier("JIO");
+              // setCallInfo([{ "cid": 46771211, "connectionType": "LTE", "eNodeB": 182700, "earfcn": 1301, "localCellId": 11, "mcc": 404, "mnc": 45, "pci": 259, "servingCellFlag": true, "tac": 8279 }, { "cid": 2147483647, "connectionType": "LTE", "eNodeB": 8388607, "earfcn": 1301, "localCellId": 255, "mcc": 2147483647, "mnc": 2147483647, "pci": 392, "servingCellFlag": false, "tac": 2147483647 }, { "cid": 2147483647, "connectionType": "GSM", "lac": 2147483647, "mcc": 2147483647, "mnc": 2147483647, "psc": 2147483647 }])
+              ToastAndroid.show("Updated...", ToastAndroid.SHORT);
+            })
+          });
+        }
+      });
+    } else {
+      ToastAndroid.show("Connect to mobile network...", ToastAndroid.SHORT);
+    }
+  }
+
+  async function requestPermissions(callback) {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ]);
+
+      if (
+        granted["android.permission.READ_PHONE_STATE"] === PermissionsAndroid.RESULTS.GRANTED &&
+        granted["android.permission.ACCESS_COARSE_LOCATION"] === PermissionsAndroid.RESULTS.GRANTED &&
+        granted["android.permission.ACCESS_FINE_LOCATION"] === PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.log("Permissions granted");
+        callback(true);
+      } else {
+        callback(false);
+        console.log("Permissions denied");
+      }
+    } catch (err) {
+      callback(false);
+      console.warn(err);
+    }
+  }
+
+  const renderCellInfo = () => (
+    <View>
+      <Text style={styleSheet.Title}>Cell Info</Text>
+      <View style={styleSheet.DetailsContainer}>
+        <Text style={{ ...styleSheet.DetailsText, marginVertical: 10 }}>Carrier: {carrier}</Text>
+        {cellInfo.map((item, index) => {
+          const { connectionType = "", tac = "NA", lac = "NA" } = item;
+          return (
+            <View key={index}>
+              <Text style={{ display: connectionType == "LTE" ? 'flex' : 'none', ...styleSheet.DetailsText }}>
+                TAC: {tac}
+              </Text>
+              <Text style={{ display: connectionType == "GSM" ? 'flex' : 'none', ...styleSheet.DetailsText }}>
+                LAC: {lac}
+              </Text>
+            </View>
+          )
+        })}
+        <View style={{ marginVertical: 10 }} />
+        <Button title="Refresh" onPress={fetchTacAndLac} color={"black"} />
+      </View>
+    </View>
+  )
+
+  const renderNoNetworkAndWifiView = message => (
+    <View style={styleSheet.DetailsContainer}>
+      <Text style={{ ...styleSheet.Title, marginTop: 0, }}>{message}</Text>
+      <Text style={{ ...styleSheet.DetailsText, marginVertical: 15, textAlign: "center" }}>Connection: {networkInfo.type}</Text>
+      <Button title="Refresh" onPress={fetchTacAndLac} color={"black"} />
+    </View>
+  )
+
+  return (
+    <View style={styleSheet.MainContainer}>
+      {networkInfo.isConnected ? networkInfo.type != "wifi" ? renderCellInfo() : renderNoNetworkAndWifiView("Switch from Wifi to Mobile Network and refresh...") : renderNoNetworkAndWifiView("Connect to internet and refresh...")}
+    </View>
+  )
+}
+
+const styleSheet = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    backgroundColor: '#c1bfbf'
+  },
+  Title: {
+    color: '#646262',
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: '5%',
+  },
+  DetailsContainer: {
+    margin: "6%",
+    backgroundColor: "white",
+    padding: '5%',
+    elevation: 9,
+    borderRadius: 8
+  },
+  DetailsText: {
+    fontSize: 18,
+    fontWeight: '700'
+  }
+});
+
+export default App;
---
+++ android/app/src/main/java/com/cellinfo/telephony/TelephonyPackage.java
@@ -0,0 +1,32 @@
+package com.cellinfo.telephony;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Rafael on 03/07/17.
+ */
+
+public class TelephonyPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new TelephonyModule(reactContext));
+
+        return modules;
+    }
+}
